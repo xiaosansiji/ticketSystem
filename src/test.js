90d7a53a14f79b119b1fad0af7aa908e9312cbc4
wuding.sz <wuding.sz@antfin.com>
2020-04-01 00:13:14 +0800

    feat: 一次选座失败时，尝试在其他区域内选座

--- src/test.js
+++ src/test.js
@@ -13,30 +13,44 @@ function getRandomArea(areas) {
   return areas[Math.floor(Math.random()*areas.length)];
 }
 
-console.log(getRandomArea(['A', 'B', 'C', 'D']));
+function getRandomSeat() {
+  
+}
 
 // 测试多次购票
 function test() {
   // 总售出票数
   let totalSelled = 0;
-  for (let n = 1; n <= 2000; n++) {
-    const areas = ['A', 'B', 'C', 'D'];
-    const area = getRandomArea(areas);
+  for (let n = 1; n <= 3000; n++) {
+    let areas = ['A', 'B', 'C', 'D'];
+    let area = '';
     // 每次取票张数
     const size = utils.getRandom(1, 5);
+    let seats = [];
     console.log(`***第 ${n} 次，本次购票 ${size} 张: ***`);
-    const seats = allSeats[area].get(size);
+  
+    // 选座失败时，尝试在其他区域内选座
+    while(areas.length) {
+      if(seats && seats.length) {
+        break;
+      } else {
+        area = getRandomArea(areas);
+        seats = allSeats[area].get(size);
+        areas.splice(areas.indexOf(area), 1);
+      }
+    }
+
     if(seats && seats.length) {
       console.log(seats);
       for(const seat of seats) {
         const { line, column }= utils.getPostion(seat);
         console.log(`${area} 区: ${line} 排, ${column} 座`);
       }
+      totalSelled += seats.length;
     } else {
       console.log('购票失败');
     }
     
-    totalSelled += seats.length;
     console.log(`已售出 ${totalSelled} 张票`);
     console.log(`总剩余 ${1950 * 4 - totalSelled} 张票`);
   }
